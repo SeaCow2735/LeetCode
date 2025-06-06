4c91d92fcea1bffe5a34cdaad691350bc6fb9d96
SeaCow2735 <thisisaseacow@gmail.com>
2025-06-06 23:08:12 +0700

    thu 6 6/6/2025

---
+++ Remove_Element/Remove_Element/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Remove_Element
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+        }
+        public class Solution {
+            public int RemoveElement(int[] nums, int val) {
+                if (nums == null || nums.Length == 0)
+                    return 0;
+                int k = 0; 
+
+                for (int i = 0; i < nums.Length; i++) {
+                    if (nums[i] != val) {
+                        nums[k] = nums[i]; 
+                        k++;
+                    }
+                }
+                return k;
+            }
+            
+        }
+        
+    }
+}
---
+++ remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+
+namespace remove_duplicates_from_sorted_array
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+        public class Solution {
+            public int RemoveDuplicates(int[] nums) {
+                if (nums == null || nums.Length == 0)
+                    return 0;
+
+                int k = 1; 
+
+                for (int i = 1; i < nums.Length; i++) {
+                    if (nums[i] != nums[k - 1]) {
+                        nums[k] = nums[i]; 
+                        k++;
+                    }
+                }
+
+                return k;
+            }
+        }
+
+    }
+}
