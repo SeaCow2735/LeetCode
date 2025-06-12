0b86a5d84385211e822f82c9c5f3f355aeb0e4bf
SeaCow2735 <thisisaseacow@gmail.com>
2025-06-12 20:01:31 +0700

    9/6/2025

---
+++ sqrt/sqrt/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace sqrt
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+        public class Solution {
+            public int MySqrt(int x) {
+                for(int i=1;i<=x/2;i++)
+                {
+                    if (i * i == x)
+                    {
+                        return i;
+                    }
+                    else if (i * i > x)
+                    {
+                        return i-1;
+                    }
+                }
+                if(x<2)
+                return x;
+            }
+        }
+    }
+}
