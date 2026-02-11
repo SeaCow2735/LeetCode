efa1fcdde7f38f121255a737c6654762860927f1
Nguyễn Phạm Hùng Nhân <thisisaseacow@gmail.com>
2026-02-11 20:46:18 +0700

    Contains_Duplicate, Happy_Number, Isomorphic_Strings, Power_of_Two

---
+++ HappyNumber/HappyNumber/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace HappyNumber
+{
+    internal class Program
+    {
+        public class Solution
+        {
+            public bool IsHappy(int n)
+            {
+                if (squareSum(n) == 1)
+                    return true;
+                else
+                {
+                    while (squareSum(n) != 1)
+                    {
+                        n = squareSum(n);
+                        if (n == 4)
+                            return false;
+                    }
+                    return true;
+                }
+            }
+            public int squareSum(int n)
+            {
+                int sum = 0;
+                while (n > 0)
+                {
+                    int digit = n % 10;
+                    sum += digit * digit;
+                    n /= 10;
+                }
+                sum += n*n;
+                return sum;
+            }
+        }
+        public static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            int n;
+            Console.WriteLine("Enter a number:");
+            n = Convert.ToInt32(Console.ReadLine());
+            bool result = solution.IsHappy(n);
+            Console.WriteLine("Input: n = " + n);
+            Console.WriteLine(result);
+        }
+        
+    }
+}
---
+++ Isomorphic Strings/Isomorphic Strings/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace IsomorphicStrings;
+public class Solution
+{
+    public bool IsIsomorphic(string s, string t)
+    {
+        int[] mapS = new int[256];
+        int[] mapT = new int[256];
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (mapS[s[i]] != mapT[t[i]])
+            {
+                return false;
+            }
+            mapS[s[i]] = i + 1;
+            mapT[t[i]] = i + 1;
+        }
+        return true;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Solution s = new Solution();
+        string str1 = Console.ReadLine();
+        string str2 = Console.ReadLine();
+        Console.WriteLine(s.IsIsomorphic(str1,str2));
+       
+    }
+}
