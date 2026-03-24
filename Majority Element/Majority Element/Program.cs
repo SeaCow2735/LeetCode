7ea8c76564ac70682ab7d061cacb9b423e7eb0aa
Nguyễn Phạm Hùng Nhân <thisisaseacow@gmail.com>
2026-03-24 22:27:03 +0700

    Majority Element

---
+++ Majority Element/Majority Element/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Majority_Element
+{
+    internal class Program
+    {   //Best solution
+        public class Solution {
+            public int MajorityElement(int[] nums) {
+                int ele = nums[0];
+                int count = 1;
+
+                for (int i = 1; i < nums.Length; i++)
+                {
+                    if (count == 0)
+                    {
+                        ele = nums[i];
+                        count = 1;
+                    }
+                    else if (ele == nums[i])
+                    {
+                        count++;
+                    }
+                    else
+                    {
+                        count--;
+                    }
+                }
+
+                return ele;
+            }
+        }
+
+        public static void Main(string[] args)
+        {
+            
+        }
+    }
+}
+
