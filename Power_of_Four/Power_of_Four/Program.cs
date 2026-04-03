3c0c2e3bf179fd63d4742b19954e6d6cf119dc4f
Nhan Nguyen <thisisaseacow@gmail.com>
2026-04-03 22:47:27 +0700

    Power of Four

---
+++ Power_of_Four/Power_of_Four/Program.cs
@@ -0,0 +1,38 @@
+﻿//My Solution
+/*public class Solution
+{
+    public bool IsPowerOfFour(int n)
+    {
+        if (n <= 0)
+            return false;
+        for (int i = 0; i <= Math.Sqrt(n); i++)
+        {
+            if (Math.Pow(4, i) == n)
+                return true;
+        }
+        return false;
+    }
+}*/
+
+//Best Solution
+public class Solution
+{
+    public bool IsPowerOfFour(int n)
+    {
+        if (n <= 0)
+        {
+            return false;
+        }
+        else
+        {
+            if ((n & (n - 1)) == 0 && n % 3 == 1)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
