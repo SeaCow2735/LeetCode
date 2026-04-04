57c405d3fc11fcd04770d8cfcccfa2c64940ac52
Nhan Nguyen <thisisaseacow@gmail.com>
2026-04-04 22:30:35 +0700

    To Lower Case

---
+++ To_Lower_Case/To_Lower_Case/Program.cs
@@ -0,0 +1,33 @@
+﻿//My Solution
+/*public class Solution
+{
+    public string ToLowerCase(string s)
+    {
+        return s.ToLowerInvariant();
+    }
+}*/
+//Best Solution
+/*public class Solution
+{
+    public string ToLowerCase(string s)
+    {
+        int len = s.Length;
+        StringBuilder strBuilder = new StringBuilder();
+        for (int i = 0; i < len; i++)
+        {
+            int ascii = (int)s[i];
+            if (ascii >= 65 && ascii <= 90)
+            {
+                ascii += 32;
+                char a = (char)ascii;
+                strBuilder.Append(a);
+            }
+            else
+            {
+                char a = (char)ascii;
+                strBuilder.Append(a);
+            }
+        }
+        return strBuilder.ToString();
+    }
+}*/
