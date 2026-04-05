f0b9a17164ff05fd56198c5674581503d18b244f
Nhan Nguyen <thisisaseacow@gmail.com>
2026-04-05 17:58:04 +0700

    Remove Duplicates From Sorted List

---
+++ Remove_Duplicates_From_Sorted_List/Remove_Duplicates_From_Sorted_List/Program.cs
@@ -0,0 +1,32 @@
+﻿//My Solution (Best Solution)
+
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+/*public class Solution
+{
+    public ListNode DeleteDuplicates(ListNode head)
+    {
+        ListNode res = head;
+        while (head != null && head.next != null)
+        {
+            if (head.val == head.next.val)
+            {
+                head.next = head.next.next;
+            }
+            else
+            {
+                head = head.next;
+            }
+        }
+        return res;
+    }
+}*/
