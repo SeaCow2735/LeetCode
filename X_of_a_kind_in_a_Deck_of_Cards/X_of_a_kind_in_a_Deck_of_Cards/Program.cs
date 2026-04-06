043fccb81d8ab57b85521cb1f6a87c8d6c7ecd2b
Nhan Nguyen <thisisaseacow@gmail.com>
2026-04-06 22:08:28 +0700

    X of a kind in a Deck Of Cards

---
+++ X_of_a_kind_in_a_Deck_of_Cards/X_of_a_kind_in_a_Deck_of_Cards/Program.cs
@@ -0,0 +1,67 @@
+﻿//My solution 
+/*public class Solution
+{
+    public bool HasGroupsSizeX(int[] deck)
+    {
+        Dictionary<int, int> counts = new Dictionary<int, int>();
+
+        foreach (int n in deck)
+        {
+
+            counts.TryGetValue(n, out int c);
+            counts[n] = c + 1;
+        }
+
+        int x = 0;
+
+        foreach (int c in counts.Values)
+        {
+
+            x = gcd(x, c);
+        }
+
+        return x != 1;
+    }
+
+    private static int gcd(int a, int b)
+    {
+        if (a < b) return gcd(b, a);
+        if (b == 0) return a;
+
+        return gcd(b, a % b);
+    }
+}*/
+
+//Best Solution
+public class Solution
+{
+    public bool HasGroupsSizeX(int[] deck)
+    {
+        if (deck.Length < 2)
+            return false;
+
+        int max = deck[0];
+        for (int i = 1; i < deck.Length; i++)
+            if (deck[i] > max)
+                max = deck[i];
+        int[] count = new int[max + 1];
+        for (int i = 0; i < deck.Length; i++)
+            count[deck[i]]++;
+        for (int x = 2; x <= deck.Length; x++)
+        {
+            bool can = true;
+            for (int i = 0; i < count.Length; i++)
+            {
+                if (count[i] > 0 && count[i] % x != 0)
+                {
+                    can = false;
+                    break;
+                }
+            }
+            if (can)
+                return true;
+        }
+
+        return false;
+    }
+}
