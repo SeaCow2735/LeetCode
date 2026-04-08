54481c0ec9022c938b8d11fbe77b48d6f16d1acc
Nhan Nguyen <thisisaseacow@gmail.com>
2026-04-08 17:25:36 +0700

    Reverse Only Letters

---
+++ reverse_only_letters/reverse_only_letters/Program.cs
@@ -0,0 +1,26 @@
+﻿public class Solution
+{
+    public string ReverseOnlyLetters(string s)
+    {
+        char[] arr = s.ToCharArray();
+        int start = 0, end = arr.Length - 1;
+        while (start < end)
+        {
+            if (char.IsLetter(arr[start]) && char.IsLetter(arr[end]))
+            {
+                (arr[start], arr[end]) = (arr[end], arr[start]);
+                start++;
+                end--;
+            }
+            else if (!char.IsLetter(arr[start]))
+            {
+                start++;
+            }
+            else
+            {
+                end--;
+            }
+        }
+        return new string(arr);
+    }
+}
