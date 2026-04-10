1a2fab3792b369d8e3e14446e760ba2da716016d
Nhan Nguyen <thisisaseacow@gmail.com>
2026-04-10 20:54:32 +0700

    Two Sum (Two Pointer)

--- 1. Two Sum/1. Two Sum/Program.cs
+++ 1. Two Sum/1. Two Sum/Program.cs
@@ -1,4 +1,4 @@
-﻿namespace _1._Two_Sum
+﻿/*namespace _1._Two_Sum
 {
     internal class Program
     {
@@ -17,6 +17,56 @@
                     }
                 }
                 return null;
+            }
+        }
+    }
+}*/
+
+
+namespace _1._Two_Sum
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+        }
+        public class Solution
+        {
+            public int[] TwoSum(int[] nums, int target)
+            {
+                var indexedNums = new int[nums.Length][];
+                for (int i = 0; i < nums.Length; i++)
+                {
+                    indexedNums[i] = new int[] { nums[i], i };
+                }
+
+                
+                Array.Sort(indexedNums, (a, b) => a[0].CompareTo(b[0]));
+
+                
+                int left = 0;
+                int right = nums.Length - 1;
+
+                while (left < right)
+                {
+                    int currentSum = indexedNums[left][0] + indexedNums[right][0];
+
+                    if (currentSum == target)
+                    {
+                        
+                        return new int[] { indexedNums[left][1], indexedNums[right][1] };
+                    }
+                    else if (currentSum > target)
+                    {
+                        right--;
+                    }
+                    else
+                    {
+                        left++;
+                    }
+                }
+
+                return Array.Empty<int>();
             }
         }
     }
