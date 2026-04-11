e158810ff5e4701e6e387f15c8e81fe56a5c9e2e
Nhan Nguyen <thisisaseacow@gmail.com>
2026-04-11 19:59:18 +0700

    remove linked list elements

---
+++ RemoveLinkedListElements/RemoveLinkedListElements/Program.cs
@@ -0,0 +1,32 @@
+﻿/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution
+{
+    public ListNode RemoveElements(ListNode head, int val)
+    {
+        ListNode ans = new ListNode(0, head);
+        ListNode dummy = ans;
+
+        while (dummy.next != null)
+        {
+            if (dummy.next.val == val)
+            {
+                dummy.next = dummy.next.next;
+            }
+            else
+            {
+                dummy = dummy.next;
+            }
+        }
+        return ans.next;
+    }
+}
