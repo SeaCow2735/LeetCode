b60bae1185f41166fbd5452267d562be553ee0db
Nhan Nguyen <thisisaseacow@gmail.com>
2026-04-14 23:52:35 +0700

    Min Cost Climbing Stairs

---
+++ Min_Cost_Climbing_Stairs/Min_Cost_Climbing_Stairs/Program.cs
@@ -0,0 +1,27 @@
+﻿// My Solution ( Best Solution )
+
+
+public class Solution
+{
+    public int MinCostClimbingStairs(int[] cost)
+    {
+        int n = cost.Length;
+
+        if (n == 1)
+        {
+            return cost[0];
+        }
+
+        int[] dp = new int[n];
+
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+
+        for (int i = 2; i < n; i++)
+        {
+            dp[i] = Math.Min(dp[i - 1], dp[i - 2]) + cost[i];
+        }
+
+        return Math.Min(dp[n - 1], dp[n - 2]);
+    }
+}
