7fc7706ca67c8343b6abbdd5b924ca46241ff7c4
Nhan Nguyen <thisisaseacow@gmail.com>
2026-04-15 22:15:10 +0700

    Jewels and Stones

---
+++ Jewels and Stones/Jewels and Stones/Program.cs
@@ -0,0 +1,39 @@
+﻿// My first solution to the problem "Jewels and Stones" on LeetCode.
+/*public class Solution
+{
+    public int NumJewelsInStones(string jewels, string stones)
+    {
+        char[] jews = jewels.ToCharArray();
+        int count = 0;
+        foreach (char s in stones)
+        {
+            foreach (char c in jews)
+            {
+                if (s == c)
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}*/
+
+
+//My best solution to the problem "Jewels and Stones" on LeetCode.
+public class Solution
+{
+    public int NumJewelsInStones(string jewels, string stones)
+    {
+        var jewelSet = new HashSet<char>(jewels);
+        int count = 0;
+        foreach (char s in stones)
+        {
+            if (jewelSet.Contains(s))
+            {
+                count++;
+            }
+        }
+        return count;
+    }
+}
