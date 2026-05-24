5f7c2b04a717969ee53d74b430bc16ed5ad762f0
Nhan Nguyen <thisisaseacow@gmail.com>
2026-05-24 22:20:41 +0700

    1512. Number of Good Pairs

---
+++ 1512.Number_of_Good_Pairs/1512.Number_of_Good_Pairs/Program.cs
@@ -0,0 +1,20 @@
+﻿// 1512. Number of Good Pairs
+// My solution (best runtime solution)
+public class Solution
+{
+    public int NumIdenticalPairs(int[] nums)
+    {
+        int count = 0;
+        for(int i = 0; i < nums.Length-1; i++)
+        {
+           for(int j = i+1;j<nums.Length;j++)
+            {
+                if (nums[i] == nums[j] && i < j)
+                {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+}
