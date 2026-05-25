98f178379ac19cab4ae247bf4a9c4bb90f5b9974
Nhan Nguyen <thisisaseacow@gmail.com>
2026-05-25 23:29:45 +0700

    1672. Richest Customer Wealth

---
+++ 1672.Richest_Customer_Wealth/1672.Richest_Customer_Wealth/Program.cs
@@ -0,0 +1,39 @@
+﻿// 1672. Richest Customer Wealth
+// My solution (not the best runtime solution)
+public class Solution
+{
+    public int MaximumWealth(int[][] accounts)
+    {
+        int sum = 0;
+        int rich = 0;
+        foreach (var account in accounts)
+        {
+            foreach (var wealth in account)
+            {
+                sum += wealth;
+            }
+            rich = Math.Max(sum, rich);
+            sum = 0;
+        }
+        return rich;
+    }
+}
+
+// Best runtime solution 
+/*public class Solution
+{
+    public int MaximumWealth(int[][] accounts)
+    {
+        int maxSum = 0;
+        for (int i = 0; i < accounts.Length; i++)
+        {
+            int localSum = 0;
+            for (int j = 0; j < accounts[i].Length; j++)
+            {
+                localSum += accounts[i][j];
+            }
+            maxSum = (localSum > maxSum) ? localSum : maxSum;
+        }
+        return maxSum;
+    }
+}*/
