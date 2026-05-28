f698fd43ab4409ada051835ffcefb291333aeefe
Nhan Nguyen <thisisaseacow@gmail.com>
2026-05-28 20:53:39 +0700

    3898. Find the Degree of Each Vertex

---
+++ 3898.Find_the_Degree_of_Each_Vertex/3898.Find_the_Degree_of_Each_Vertex/Program.cs
@@ -0,0 +1,25 @@
+﻿// 3898. Find the Degree of Each Vertex
+// My solution (best runtime solution)
+using System;
+
+public class Solution
+{
+    public int[] FindDegrees(int[][] matrix)
+    {
+        int[] result = new int[matrix.Length];
+
+        for (int i = 0; i < matrix.Length; i++)
+        {
+            int sum = 0;
+
+            foreach (int num in matrix[i])
+            {
+                sum += num;
+            }
+
+            result[i] = sum;
+        }
+
+        return result;
+    }
+}
