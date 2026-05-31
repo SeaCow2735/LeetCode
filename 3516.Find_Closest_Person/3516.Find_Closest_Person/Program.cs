68b4e616d2c90ffc7742a5ac30da74cc6ff85741
Nhan Nguyen <thisisaseacow@gmail.com>
2026-05-31 21:17:46 +0700

    3516. Find Closest Person

---
+++ 3516.Find_Closest_Person/3516.Find_Closest_Person/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Solution
+{
+    public int FindClosest(int x, int y, int z)
+    {
+        int count1 = 0;
+        int count2 = 0;
+        count1 = Math.Abs(x-z);
+        count2 = Math.Abs(y-z);
+        if (count1 > count2)
+            return 2;
+        if (count2 > count1)
+            return 1;
+        else
+            return 0;
+    }
+}
