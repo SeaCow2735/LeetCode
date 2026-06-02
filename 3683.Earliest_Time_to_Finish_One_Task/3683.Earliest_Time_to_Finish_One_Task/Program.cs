070e54b41668a7377acb3c2ddf0532df5668601e
Nhan Nguyen <thisisaseacow@gmail.com>
2026-06-02 19:14:55 +0700

    3683. Earliest Time to Finish One Task

---
+++ 3683.Earliest_Time_to_Finish_One_Task/3683.Earliest_Time_to_Finish_One_Task/Program.cs
@@ -0,0 +1,16 @@
+﻿//3683. Earliest Time to Finish One Task
+// My solution (the best runtime solution)
+public class Solution
+{
+    public int EarliestTime(int[][] tasks)
+    {
+        int result = tasks[0][0] + tasks[0][1];
+        for (int i = 1; i < tasks.Length; i++)
+        {
+            int[] currentTask = tasks[i];
+            result = Math.Min(result, currentTask[0] + currentTask[1]);
+        }
+
+        return result;
+    }
+}
