68175bc44c065e0fa8bc2c5f1c18a20915218f3d
Nhan Nguyen <thisisaseacow@gmail.com>
2026-06-04 23:44:20 +0700

    2974. Minimum Number Game and  2942.Find_Words_Containing_Character

---
+++ 2942.Find_Words_Containing_Character/2942.Find_Words_Containing_Character/Program.cs
@@ -0,0 +1,20 @@
+﻿// 2942. Find Words Containing Character
+// My solution (the best runtime solution)
+public class Solution
+{
+    public IList<int> FindWordsContaining(string[] words, char x)
+    {
+        var result = new List<int>();
+        for (int i = 0; i < words.Length; i++)
+        {
+            if (words[i].Contains(x))
+            {
+                result.Add(i);
+            }
+        }
+        return result;
+    }
+}
+
+
+
---
+++ 2974.Minimum_Number_Game/2974.Minimum_Number_Game/Program.cs
@@ -0,0 +1,18 @@
+﻿//2974. Minimum Number Game
+// My solution (the best solution for runtime)
+public class Solution
+{
+    public int[] NumberGame(int[] nums)
+    {
+        int temp = 0;
+        nums.Sort();
+        for (int i = 0; i <= nums.Length-1;i=i+2)
+        {
+           temp = nums[i];
+           nums[i] = nums[i+1];
+            nums[i + 1] = temp;
+            temp = 0;
+        }
+        return nums;
+    }
+}
