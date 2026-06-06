428268cc1208e38dbc7b4d1a827b08833bc81019
Nhan Nguyen <thisisaseacow@gmail.com>
2026-06-06 23:07:55 +0700

    1470. Shuffle the Array

---
+++ 1470.Shuffle_the_Array/1470.Shuffle_the_Array/Program.cs
@@ -0,0 +1,16 @@
+﻿//1470. Shuffle the Array
+//My solution
+public class Solution
+{
+    public int[] Shuffle(int[] nums, int n)
+    {
+        int[] res = new int[nums.Length];
+        int index = 0;
+        for (int i = 0; i < n; i++)
+        {
+            res[index++] = nums[i];
+            res[index++] = nums[n + i];
+        }
+        return res;
+    }
+}
