4a6cd71792b427761a91fcd019aa4e18fa9c354b
Nhan Nguyen <thisisaseacow@gmail.com>
2026-06-07 18:55:21 +0700

    2413. Smallest Even Multiple

---
+++ 2413.Smallest_Even_Multiple/2413.Smallest_Even_Multiple/Program.cs
@@ -0,0 +1,16 @@
+﻿//2413. Smallest Even Multiple
+//My solution (the best runtime solution)
+public class Solution
+{
+    public int SmallestEvenMultiple(int n)
+    {
+        if (n % 2 == 0)
+        {
+            return n;
+        }
+        else
+        {
+            return 2 * n;
+        }
+    }
+}
