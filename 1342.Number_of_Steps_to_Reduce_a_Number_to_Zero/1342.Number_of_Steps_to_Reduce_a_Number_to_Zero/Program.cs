b85a9e7079dd408de93b56f85ada956ae465176e
Nhan Nguyen <thisisaseacow@gmail.com>
2026-06-09 22:42:05 +0700

    1342. Number of Steps to Reduce a Number to Zero

---
+++ 1342.Number_of_Steps_to_Reduce_a_Number_to_Zero/1342.Number_of_Steps_to_Reduce_a_Number_to_Zero/Program.cs
@@ -0,0 +1,24 @@
+﻿// 1342. Number of Steps to Reduce a Number to Zero
+// My solution (the best runtime solution)
+public class Solution
+{
+    public int NumberOfSteps(int num)
+    {
+        int count = 0;
+        while(num!=0)
+        {
+            if (num % 2 == 0)
+            {
+                count++;
+                num = num / 2;
+            }
+            else
+            {
+                num--;
+                count++;
+            }
+        }
+        return count;
+
+    }
+}
