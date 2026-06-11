5928db11bf221a8710f2ef4fc6b5c53e8605ab5f
Nhan Nguyen <thisisaseacow@gmail.com>
2026-06-11 21:40:33 +0700

    2520. Count the Digits That Divide a Number

---
+++ 2520.Count_the_Digits_That_Divide_a_Number/2520.Count_the_Digits_That_Divide_a_Number/Program.cs
@@ -0,0 +1,21 @@
+﻿//2520. Count the Digits That Divide a Number
+//My solution (the best runtime solution)
+public class Solution
+{
+    public int CountDigits(int num)
+    {
+        int temp = num;
+        int temp2 = 0;
+        int count = 0;
+        while(temp!=0)
+        {
+            temp2 = temp % 10;
+            if(num%temp2==0)
+            {
+                count++;
+            }
+            temp = temp / 10;
+        }
+        return count;
+    }
+}
