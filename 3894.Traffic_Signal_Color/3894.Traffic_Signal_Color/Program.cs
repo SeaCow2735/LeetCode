5af1e65804b374e99e65683a762ebf1a6779676a
Nhan Nguyen <thisisaseacow@gmail.com>
2026-06-12 18:27:53 +0700

    3894. Traffic Signal Color

---
+++ 3894.Traffic_Signal_Color/3894.Traffic_Signal_Color/Program.cs
@@ -0,0 +1,16 @@
+﻿//3894. Traffic Signal Color
+//My solution (the best runtime solution)
+public class Solution
+{
+    public string TrafficSignal(int timer)
+    {
+        if (timer == 0)
+            return "Green";
+        if (timer == 30)
+            return "Orange";
+        if (timer > 30 && timer <= 90)
+            return "Red";
+        else
+            return "Invalid";
+    }
+}
