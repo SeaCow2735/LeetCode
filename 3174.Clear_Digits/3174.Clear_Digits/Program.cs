403a528e93e7a95a46878e1016ba6604cb381abf
Nhan Nguyen <thisisaseacow@gmail.com>
2026-06-14 22:33:20 +0700

    3174. Clear Digits

---
+++ 3174.Clear_Digits/3174.Clear_Digits/Program.cs
@@ -0,0 +1,26 @@
+﻿//3174. Clear Digits
+//My solution (the best runtime solution)
+using System.Text;
+
+public class Solution
+{
+    public string ClearDigits(string s)
+    {
+        StringBuilder sb = new StringBuilder();
+
+        foreach (char ch in s)
+        {
+            if (char.IsDigit(ch))
+            {
+                if (sb.Length > 0)
+                    sb.Remove(sb.Length - 1, 1);
+            }
+            else
+            {
+                sb.Append(ch);
+            }
+        }
+
+        return sb.ToString();
+    }
+}
