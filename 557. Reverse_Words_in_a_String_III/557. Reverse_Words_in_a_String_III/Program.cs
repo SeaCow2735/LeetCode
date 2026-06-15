ef2b2b9127b19974c30fcd49b6a240bc58aeaa3e
Nhan Nguyen <thisisaseacow@gmail.com>
2026-06-15 21:17:05 +0700

    557. Reverse Words in a String III

---
+++ 557. Reverse_Words_in_a_String_III/557. Reverse_Words_in_a_String_III/Program.cs
@@ -0,0 +1,38 @@
+﻿//My Solution
+public class Solution
+{
+    public string ReverseWords(string s)
+    {
+        string[] words = s.Split();
+        string[] reversedWords = new string[words.Length];
+
+        for (int i = 0; i < words.Length; i++)
+        {
+            char[] wordChars = words[i].ToCharArray();
+            Array.Reverse(wordChars);
+            reversedWords[i] = new string(wordChars);
+        }
+
+        return string.Join(" ", reversedWords);
+    }
+}
+
+//Best solution
+public class Solution
+{
+    public string ReverseWords(string s)
+    {
+        var array = s.ToCharArray();
+        var fInd = 0;
+        for (int i = 0; i < s.Length; i++)
+        {
+            if (i + 1 == s.Length || s[i + 1] == ' ')
+            {
+                Array.Reverse(array, fInd, i - fInd + 1);
+                fInd = i + 2;
+            }
+        }
+
+        return new string(array);
+    }
+}
