61e9846f1f03ee1537686575bcc086c3583a33e5
Nhan Nguyen <thisisaseacow@gmail.com>
2026-06-16 22:09:05 +0700

    1716. Calculate Money in Leetcode Bank

---
+++ 1716.Calculate_Money_in_Leetcode_Bank/1716.Calculate_Money_in_Leetcode_Bank/Program.cs
@@ -0,0 +1,22 @@
+﻿//My Solution
+public class Solution
+{
+    public int TotalMoney(int n)
+    {
+        int total = 0;
+        int lastMonday = 0;
+        int current = 1;
+
+        for (int i = 1; i <= n; i++)
+        {
+            if (i % 7 == 1)
+            {
+                lastMonday++;
+                current = 0;
+            }
+            total = total + lastMonday + current;
+            current++;
+        }
+        return total;
+    }
+}
