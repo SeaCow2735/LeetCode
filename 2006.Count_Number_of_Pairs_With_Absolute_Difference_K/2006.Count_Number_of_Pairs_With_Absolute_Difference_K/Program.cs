096e82a5a773f44d70e8ab6aba45e3c2f92ec0ff
Nhan Nguyen <thisisaseacow@gmail.com>
2026-06-21 18:52:21 +0700

    2006. Count Number of Pairs With Absolute Difference K

---
+++ 2006.Count_Number_of_Pairs_With_Absolute_Difference_K/2006.Count_Number_of_Pairs_With_Absolute_Difference_K/Program.cs
@@ -0,0 +1,44 @@
+﻿//2006. Count Number of Pairs With Absolute Difference K
+//My Solution 
+public class Solution
+{
+    public int CountKDifference(int[] nums, int k)
+    {
+        nums.Sort();
+        int count = 0;
+        for(int i = 0; i < nums.Length; i++)
+        {
+            for(int j = i+1; j < nums.Length; j++)
+            {
+                    if (Math.Abs(nums[i] - nums[j]) == k)
+                    {
+                        count++;
+                    }
+            }
+        }
+        return count;
+    }
+}
+//Best runtime solution
+/*public class Solution
+{
+    public int CountKDifference(int[] nums, int k)
+    {
+        int c = 0;
+        int[] fre = new int[101];
+        foreach (int num in nums)
+        {
+            if (num - k >= 0)
+            {
+                c += fre[num - k];
+            }
+            if (num + k <= 100)
+            {
+                c += fre[num + k];
+            }
+            fre[num]++;
+        }
+        return c;
+
+    }
+}*/
