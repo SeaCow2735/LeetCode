957f5bdd0530539c3e29724207ac239eb2aac3db
Nhan Nguyen <thisisaseacow@gmail.com>
2026-06-22 21:29:09 +0700

    3684. Maximize Sum of At Most K Distinct Elements

---
+++ 3684.Maximize_Sum_of_At_Most_K_Distinct_Elements/3684.Maximize_Sum_of_At_Most_K_Distinct_Elements/Program.cs
@@ -0,0 +1,47 @@
+﻿//3684. Maximize Sum of At Most K Distinct Elements
+// My Solution
+
+public class Solution
+{
+    public int[] MaxKDistinct(int[] nums, int k)
+    {
+        HashSet<int> a = new HashSet<int>();
+        Array.Sort(nums);
+        for (int i = nums.Length - 1; i >= 0; i--)
+        {
+            if (a.Count < k)
+            {
+                a.Add(nums[i]);
+            }
+            else
+            {
+                break;
+            }
+        }
+        return a.ToArray();
+    }
+}
+
+// The best runtime solution
+public class Solution
+{
+    public int[] MaxKDistinct(int[] nums, int k)
+    {
+        Array.Sort(nums, (a, b) => b.CompareTo(a));
+
+        var maxKDistinctNums = new List<int>();
+        foreach (int num in nums)
+        {
+            if (
+              maxKDistinctNums.Count > 0
+              && maxKDistinctNums[^1] == num
+            ) continue;
+
+            maxKDistinctNums.Add(num);
+
+            if (--k == 0) break;
+        }
+
+        return maxKDistinctNums.ToArray();
+    }
+}
