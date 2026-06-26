6af5979b912624a6a4b1438062e4cd7ce29fa74e
unknown <thisisaseacow@gmail.com>
2026-06-26 21:11:31 +0700

    1732. Find the Highest Altitude

---
+++ 1732.Find_the_Highest_Altitude/1732.Find_the_Highest_Altitude/Program.cs
@@ -0,0 +1,17 @@
+﻿// 1732. Find the Highest Altitude
+// My Solution (the best runtime solution)
+public class Solution
+{
+    public int LargestAltitude(int[] gain)
+    {
+        int Max = 0;
+        int temp = 0;
+        for (int i =0;i<gain.Length;i++)
+        {
+            temp+= gain[i];
+            if(temp > Max) Max = temp;
+        }
+        return Max;
+
+    }
+}
